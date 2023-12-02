521621d2131a7bd52cf38439ce3844d86e55b8ad
Sushanta <development.experimen@gmail.com>
2023-12-03 00:32:49 +0530

    File Upload using Multer
    Single file upload, multiple file upload and different file upload using multer, Getting form data using express-form-data

--- curdNodeMysql/server.js
+++ curdNodeMysql/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const multer = require("multer"); //file upload
+//const formData = require("express-form-data"); //Module to parse multipart/form data. Based on connect-multiparty
 //initialize app
 const app = express();
 
@@ -11,9 +12,27 @@ const connection = con.getConnection();
 connection.connect();
 
 //define middlware
+// Used to parse JSON bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+//multipart/form data
+//app.use(formData.parse());
 app.use(cors());
+
+//multer configuration for file upload
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./uploads");
+	},
+	filename: function (req, file, cb) {
+		//const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		//cb(null, file.fieldname + "-" + uniqueSuffix);
+		cb(null, Date.now() + "-" + file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
 
 //create routes
 app.get("/users", (req, res) => {
@@ -82,6 +101,102 @@ app.delete("/users/:id", (req, res) => {
 			});
 		}
 	});
+});
+
+//Single file upload using multer
+app.post("/fileuplod", upload.single("image"), (req, res) => {
+	const profileImage = req.file.path; // uploaded image name
+	//console.log(req.body); // upload image name
+	connection.query(
+		`INSERT INTO profile_images (image) VALUES ('${profileImage}')`,
+		(error, results, fields) => {
+			if (error) {
+				res.status(500).json({
+					success: false,
+					message: error.message,
+				});
+			} else {
+				res.status(200).json({
+					success: true,
+					message: "Profile Image Uploaded successsfully",
+				});
+			}
+		}
+	);
+});
+
+//multiple files upload using multer
+app.post("/multifileuplod", upload.array("image", 12), (req, res) => {
+	//console.log(req.files); // upload images object
+	req.files.map((curFileObj) => {
+		const profileImage = curFileObj.path; // uploaded image name
+		//console.log(curFileObj); // get single file object
+		connection.query(
+			`INSERT INTO profile_images (image) VALUES ('${profileImage}')`,
+			(error, results, fields) => {
+				if (error) {
+					res.status(500).json({
+						success: false,
+						message: error.message,
+					});
+				} else {
+					res.status(200).json({
+						success: true,
+						message: "multiple Profile Image Uploaded successsfully",
+					});
+				}
+			}
+		);
+	});
+});
+
+//multiple file upload diffrent name field using multer
+const cpUpload = upload.fields([
+	{ name: "avatar", maxCount: 1 },
+	{ name: "profile", maxCount: 2 },
+]);
+app.post("/multifileuplod-diffrent-fieldname", cpUpload, (req, res) => {
+	console.log(req.files.avatar); // upload images object
+	console.log(req.files.profile); // upload images object
+	let flag = 0;
+	if (req.files.avatar) {
+		const avatarImage = req.files.avatar.path; // uploaded image name
+		connection.query(
+			`INSERT INTO profile_images (image) VALUES ('${avatarImage}')`,
+			(error, results, fields) => {
+				if (error) {
+					res.status(500).json({
+						success: false,
+						message: error.message,
+					});
+				} else {
+					/* res.status(200).json({
+                        success: true,
+                        message: "multiple Profile Image Uploaded successsfully",
+                    }); */
+				}
+			}
+		);
+		req.files.profile.map((curFileObj) => {
+			const profileImage = curFileObj.path; // uploaded image name
+			connection.query(
+				`INSERT INTO profile_images (image) VALUES ('${profileImage}')`,
+				(error, results, fields) => {
+					if (error) {
+						res.status(500).json({
+							success: false,
+							message: error.message,
+						});
+					} else {
+						res.status(200).json({
+							success: true,
+							message: "multiple Profile Image Uploaded successsfully",
+						});
+					}
+				}
+			);
+		});
+	}
 });
 
 //connection.end();
