1063ce4fd3b65b8efaf5c0e8d493fb883cc719f4
Sushanta <development.experimen@gmail.com>
2023-12-14 23:51:24 +0530

    Bootstrap and google font add

--- react-typescript-app/src/App.tsx
+++ react-typescript-app/src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css";
 
 function App() {
-	let pStyle = {
-		fontSize: "15px",
-		color: "blue",
-	};
 	return (
 		<div className="App">
-			<h1 style={{ color: "red", fontSize: "35px" }}>
-				React Typescript Learn
-			</h1>
-			<p style={pStyle}>
+			<h1 className="text-bg-danger">React Typescript Learn</h1>
+			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
 				laudantium deleniti magnam a eos! Quas distinctio illo modi quam
 				iure nulla ratione nobis nesciunt, veritatis necessitatibus
